40e5ac1a234d15318b8f0b2b412542e5843d7f5c
jyothisridhar <jyotishridhar7@gmail.com>
2020-03-11 10:49:23 +0530

    feat: show appropriate message when no books found on search

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,28 +1,56 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Book from './Book';
 import '../App.css';
 
 class SearchBooks extends Component {
+  state = {
+    query: ''
+  }
+
+  onSearch = (query) => {
+    this.setState({
+      query: query
+    });
+    if(this.state.query){
+      this.props.onSearch(query);
+    }
+  }
+
+  updateBookShelf = (updatedBook, shelf) =>{
+    this.props.handleShelf(updatedBook, shelf);
+  }
+
   render(){
+    const response = this.props.response;
+    console.log(response);
     return (
         <div className="search-books">
           <div className="search-books-bar">
             <Link className="close-search" to="/">Close Search</Link>
             <div className="search-books-input-wrapper">
-              {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-              */}
-              <input type="text" placeholder="Search by title or author"/>
-
+              <input type="text"
+                     placeholder="Search by title or author"
+                     value={this.state.query}
+                     onChange={(e) => this.onSearch(e.target.value)}
+              />
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid"></ol>
+            <ol className="books-grid">
+            {
+              response.length === 0 ? (
+                <div>No books found</div>
+              ) :
+              (
+                response.map((book, index) => (
+                  <li key={index}>
+                    <Book book={book} onShelfChange={this.updateBookShelf}/>
+                  </li>
+                ))
+              )
+            }
+            </ol>
           </div>
         </div>
     )
